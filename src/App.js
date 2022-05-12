9ea5e6f333a60e3e5242b01a0782c8cc4dc40e03
Cami Dominguez <camiladominguez@MacBook-Air-de-Cami.local>
2022-05-11 22:21:52 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import React from "react";
+import Persons from "./components/Persons/Persons";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Persons />
+      </div>
+    </div>
+  );
+}
+
+export default App;
